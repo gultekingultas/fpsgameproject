62d8e959bf3695cc0be72f94836441b575272a56
Gültekin Gültaş <gultekingultas@hotmail.com>
2019-02-22 23:43:23 +0300

    weapon
    Created weapon script and basic weapon model.

---
+++ FpsGameProject/Assets/Scripts/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+    public enum Weapontype
+    {
+        laser,
+        shotgun,
+        launcher
+    };
+    public Weapontype weapontype;
+    // Use this for initialization
+    void Start () {
+        if (weapontype == Weapontype.laser)
+        {
+            Debug.Log("laser");
+        }
+        else if (weapontype == Weapontype.shotgun)
+        {
+            Debug.Log("shotgun");
+        }
+        else if (weapontype == Weapontype.launcher)
+        {
+            Debug.Log("Launcher");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
